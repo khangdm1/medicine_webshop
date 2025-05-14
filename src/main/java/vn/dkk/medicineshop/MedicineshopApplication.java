21572867f86dec0edcdc4874550cfcdcf62acb33
Khang Doan Minh <dmkhang27@gmail.com>
2025-05-14 23:24:26 +0700

    login

--- src/main/java/vn/dkk/medicineshop/MedicineshopApplication.java
+++ src/main/java/vn/dkk/medicineshop/MedicineshopApplication.java
@@ -3,9 +3,10 @@ package vn.dkk.medicineshop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// @SpringBootApplication
-//include >< exclude
-@SpringBootApplication(exclude = org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
+@SpringBootApplication
+// include >< exclude
+// @SpringBootApplication(exclude =
+// org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class)
 public class MedicineshopApplication {
 
 	public static void main(String[] args) {
